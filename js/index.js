283a05deb09ea1acc1155b6f551d8f5b3da63787
Ognam.Chen <Ognam@ognam.local>
2018-04-21 11:23:17 +0800

    git

--- js/index.js
+++ js/index.js
@@ -1,17 +1,21 @@
 
 var myNav = document.getElementsByClassName("my-nav")[0];
 var mySticky = myNav.offsetTop;
-
+var myNavHr = document.getElementsByClassName("my-nav-hr")[0];
 
 window.onscroll = function () {
     fixHeader();
 };
 
 function fixHeader() {
-    console.log("ofset",myNav, window.pageYOffset, mySticky)
+    console.log("ofset",myNav, window.pageYOffset, mySticky, myNavHr)
     if (window.pageYOffset >= mySticky) {
         myNav.classList.add("sticky");
+        myNavHr.style.display = "none";
+
     } else {
         myNav.classList.remove("sticky");
+        myNavHr.style.display = "block";
+
     }
 };
